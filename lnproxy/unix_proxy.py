8788d4e195e0a663484e528a5182a20bf227cfe1
willcl-ark <will8clark@gmail.com>
2019-10-25 12:30:08 +0100

    Working Unix Socket proxy with Trio
    Signed-off-by: willcl-ark <will8clark@gmail.com>

---
+++ lnproxy/unix_proxy.py
@@ -0,0 +1,74 @@
+import logging
+import socket
+
+import trio
+from util import hexdump
+
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger("PROXYSRV")
+
+SRV_SOCK = "/tmp/unix_proxy"
+CLIENT_SOCK = "/tmp/l2-regtest/unix_socket"
+RECV_BUF = 210
+
+
+async def unlink_socket(sock):
+    """Unlink a Unix Socket
+    """
+    p = trio.Path(sock)
+    try:
+        await p.unlink()
+    except OSError:
+        if await p.exists():
+            raise
+
+
+async def proxy(read_stream, write_stream):
+    """Proxy traffic from one stream to another
+    """
+    logger.debug(f"Proxy started")
+    while True:
+        try:
+            for data in read_stream.read():
+                # data = await read_stream.receive_some(RECV_BUF)
+                await write_stream.send_all(data)
+                logger.debug(f"{len(data)}B of data proxied")
+                hexdump(data)
+        except:
+            raise
+
+
+async def socket_handler(server_stream):
+    """Handles a listening socket. Makes outbound connection to proxy traffic with.
+    :arg server_stream: a trio.SocketStream for the listening socket
+    """
+    client_stream = await trio.open_unix_socket(CLIENT_SOCK)
+    try:
+        # We run both proxies in a nursery as stream.send_all() can be blocking
+        async with trio.open_nursery() as nursery:
+            nursery.start_soon(proxy, server_stream, client_stream)
+            nursery.start_soon(proxy, client_stream, server_stream)
+    except Exception as exc:
+        print(f"unix_handler: crashed: {exc}")
+
+
+async def serve_unix_socket():
+    await unlink_socket(SRV_SOCK)
+
+    # Create the listening socket
+    sock = trio.socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+    await sock.bind(SRV_SOCK)
+    sock.listen()
+    listener = trio.SocketListener(sock)
+
+    # Manage the listening with the handler
+    try:
+        await trio.serve_listeners(socket_handler, [listener])
+    except Exception as exc:
+        print(f"serve_unix_socket: crashed: {exc}")
+    finally:
+        await trio.Path.unlink(SRV_SOCK)
+
+
+trio.run(serve_unix_socket)
---
+++ lnproxy/util.py
@@ -0,0 +1,21 @@
+import logging
+
+
+logger = logging.getLogger("UTIL")
+
+
+def hexdump(data, length=16):
+    """Print a hexdump of data
+    """
+    filter = "".join([(len(repr(chr(x))) == 3) and chr(x) or "." for x in range(256)])
+    lines = []
+    digits = 4 if isinstance(data, str) else 2
+    for c in range(0, len(data), length):
+        chars = data[c : c + length]
+        hex = " ".join(["%0*x" % (digits, (x)) for x in chars])
+        printable = "".join(
+            ["%s" % (((x) <= 127 and filter[(x)]) or ".") for x in chars]
+        )
+        lines.append("%04x  %-*s  %s\n" % (c, length * 3, hex, printable))
+    result = "\n" + "".join(lines)
+    logger.debug(result)
