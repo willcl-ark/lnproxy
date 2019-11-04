efed426e71072eb6d38500cd044fa66b1b3a4f23
willcl-ark <will8clark@gmail.com>
2019-11-04 19:59:18 +0000

    Fully parse messages
    Signed-off-by: willcl-ark <will8clark@gmail.com>

--- lnproxy/unix_proxy.py
+++ lnproxy/unix_proxy.py
@@ -6,7 +6,7 @@ import trio
 
 
 logging.basicConfig(level=logging.DEBUG)
-logger = logging.getLogger("PROXYSRV")
+logger = logging.getLogger("PROXY ")
 
 SRV_SOCK = "/tmp/unix_proxy"
 CLIENT_SOCK = "/tmp/l2-regtest/unix_socket"
@@ -42,6 +42,7 @@ async def proxy(read_stream, write_stream, direction):
             if i < handshake_acts:
 
                 # during handshake pass full 50 / 66 B messages transparently for now
+                # TODO: mock these!
                 logger.debug(f"{direction} handshake message {i}")
                 message = await read_stream.receive_some(MAX_PKT_LEN)
 
@@ -49,40 +50,33 @@ async def proxy(read_stream, write_stream, direction):
 
                 # Bolt #8: Read exactly 18 bytes from the network buffer.
                 header = await read_stream.receive_some(MSG_HEADER)
-                if len(header) != 18:
+                if len(header) != MSG_HEADER:
                     logger.debug(
-                        f"{direction} could not get header length: 18 != {len(header)}"
+                        f"{direction} could not get full header length: 18 != "
+                        f"{len(header)}"
                     )
                     break
 
                 # Bolt #8: 2-byte encrypted message length
-                body_len = struct.unpack(">H", header[:2])[0]
-                logger.debug(f"Encrypted message length: {body_len}")
+                body_len = struct.unpack(">H", header[:MSG_LEN])[0]
+                logger.debug(f"Decrypted message length: {body_len}")
 
                 # Bolt #8: 16-byte MAC of the encrypted message length
                 len_mac = struct.unpack("16s", header[-16:])[0]
-                logger.debug(f"MAC of the encrypted message length: {len_mac}")
+                # logger.debug(f"MAC of the decrypted message length: {len_mac}")
 
-                # # Bolt #8: encrypted Lightning message
-                # body = await read_stream.receive_some(body_len)
-                # # unsigned char[] ('B') from openssl/aead.py:_decode
-                # body = struct.unpack(f"{body_len}B", body)
-                # logger.debug(f"Encrypted Lightning message:\n{body}")
-                #
-                # # Bolt #8: 16 Byte MAC of the Lightning message
-                # body_mac = await read_stream.receive_some(MSG_MAC)
+                # Bolt #8: (de)encrypted Lightning message
+                body = await read_stream.receive_some(body_len)
+                logger.debug(f"Decrypted Lightning message:\n{body}")
+
+                # Bolt #8: 16 Byte MAC of the Lightning message
+                body_mac = await read_stream.receive_some(MSG_MAC)
                 # logger.debug(
                 #     f"MAC of the Lightning message: {struct.unpack('16B', body_mac)}"
                 # )
-                #
-                # # re-constitute the header and body
-                # message = header + body + body_mac
 
-                # TODO: Remove and replace with above when decryption working
-                body = await read_stream.receive_some(MAX_PKT_LEN)
-                logger.debug(f"Encrypted Lightning message:\n{body}")
-
-                message = header + body
+                # re-constitute the header and body
+                message = header + body + body_mac
 
             # send to remote
             await write_stream.send_all(message)
