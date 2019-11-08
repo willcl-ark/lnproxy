f6aff101a9b909b4b4ef1387b0c2a9a150455ec6
willcl-ark <will8clark@gmail.com>
2019-11-08 11:38:22 +0000

    Refactored parse_add_update_htlc
    Signed-off-by: willcl-ark <will8clark@gmail.com>

--- lnproxy/messages.py
+++ lnproxy/messages.py
@@ -3,6 +3,8 @@ import logging
 import struct
 import subprocess
 
+from msg_types import MSG_TYPES
+
 """
 byte: an 8-bit byte
 u16: a 2 byte unsigned integer
@@ -44,6 +46,10 @@ le_onion = "<1366s"
 LN_CLI = "/Users/will/src/lightning/cli/lightning-cli"
 L2_DIR = "--lightning-dir=/tmp/l2-regtest"
 ONION_TOOL = "/Users/will/src/lightning/devtools/onion"
+ADD_UPDATE_HTLC = 128
+RECV_NODE_PUBKEY = json.loads(
+    subprocess.run([LN_CLI, L2_DIR, "getinfo"], capture_output=True).stdout.decode()
+)["id"]
 
 
 def decode_hop_data(hop_data: hex):
@@ -66,7 +72,7 @@ def decode_hop_data(hop_data: hex):
 def encode_hop_data(
     short_channel_id: hex, amt_to_forward: int, outgoing_cltv_value: int
 ) -> hex:
-    """Encode a legacy 'hop_data' payload
+    """Encode a legacy 'hop_data' payload to hex
     https://github.com/lightningnetwork/lightning-rfc/blob/master/04-onion-routing.md#legacy-hop_data-payload-format
     """
     # Bolt #7: The hop_data format is identified by a single 0x00-byte length, for
@@ -77,7 +83,7 @@ def encode_hop_data(
     hop_data += struct.pack(be_u32, outgoing_cltv_value)
     # [12*byte:padding]
     hop_data += b"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
-    logger.debug(f"Hop data created...\n{hop_data.hex()}")
+    logger.debug(f"Encoded hop data:\n{hop_data.hex()}")
     return hop_data.hex()
 
 
@@ -87,58 +93,61 @@ def deserialize_type(msg_type: bytes) -> int:
     return struct.unpack(be_u16, msg_type)[0]
 
 
-def parse_update_add_htlc(msg_payload: bytes, direction: str) -> bytes:
-    """Parse an update_add_htlc
-    """
-    if len(msg_payload) != 1450:
-        logger.debug(
-            ValueError(
-                f"update_add_htlc body length mismatch: 1452 != {len(msg_payload)}"
-            )
-        )
-
-    channel_id = struct.unpack(le_32b, msg_payload[0:32])[0]
-    id = struct.unpack(be_u64, msg_payload[32:40])[0]
-    amount_msat = struct.unpack(be_u64, msg_payload[40:48])[0]
-    payment_hash = struct.unpack(le_32b, msg_payload[48:80])[0]
-    cltv_expiry = struct.unpack(be_u32, msg_payload[80:84])[0]
-    onion = struct.unpack(le_onion, msg_payload[84:1450])[0]
-
-    logger.debug(f"Channel_id: {channel_id.hex()}")
-    logger.debug(f"ID: {id}")
-    logger.debug(f"Amount msat: {amount_msat}")
-    logger.debug(f"Payment Hash: {payment_hash.hex()}")
-    logger.debug(f"CLTV expiry: {cltv_expiry}")
-    logger.debug(f"Orig onion length: {len(onion)}")
-    logger.debug(f"Orig onion hex:\n{onion.hex()}")
-    logger.debug("Generating new onion...")
-
-    my_node_pubkey = json.loads(
-        subprocess.run([LN_CLI, L2_DIR, "getinfo"], capture_output=True).stdout.decode()
-    )["id"]
-
+def generate_new_onion(node_pubkey: hex, amount_msat: int, payment_hash: hex) -> bytes:
     final_chan_id = 0x0000000000000000
     hop_data = encode_hop_data(final_chan_id, amount_msat, 132)
+
+    logger.debug("Generating new onion using:")
+    logger.debug(
+        f"devtools/onion generate {node_pubkey}/{hop_data} --assoc-data "
+        f"{payment_hash.hex()}"
+    )
 
     gen_onion = subprocess.run(
         [
             ONION_TOOL,
             "generate",
-            f"{my_node_pubkey}/{hop_data}",
+            f"{node_pubkey}/{hop_data}",
             "--assoc-data",
             f"{payment_hash.hex()}",
         ],
         capture_output=True,
     ).stdout.decode()
     gen_onion_bytes = bytes.fromhex(gen_onion)
-    logger.debug(f"Generated onion:\n{gen_onion}\n Size: {len(gen_onion_bytes)}")
+    logger.debug(f"Generated onion! Size: {len(gen_onion_bytes)}")
+    return gen_onion_bytes
 
-    modified_payload = msg_payload[0:84]
-    modified_payload += struct.pack(le_onion, gen_onion_bytes)
 
-    if direction == "Outbound":
-        return modified_payload
-    return msg_payload
+def parse_update_add_htlc(orig_payload: bytes) -> bytes:
+    """Parse an update_add_htlc
+    """
+    if len(orig_payload) != 1450:
+        logger.debug(
+            ValueError(
+                f"update_add_htlc body length mismatch: 1450 != {len(orig_payload)}"
+            )
+        )
+
+    channel_id = struct.unpack(le_32b, orig_payload[0:32])[0]
+    _id = struct.unpack(be_u64, orig_payload[32:40])[0]
+    amount_msat = struct.unpack(be_u64, orig_payload[40:48])[0]
+    payment_hash = struct.unpack(le_32b, orig_payload[48:80])[0]
+    cltv_expiry = struct.unpack(be_u32, orig_payload[80:84])[0]
+    onion = struct.unpack(le_onion, orig_payload[84:1450])[0]
+
+    logger.debug(f"Channel_id: {channel_id.hex()}")
+    logger.debug(f"ID: {_id}")
+    logger.debug(f"Amount msat: {amount_msat}")
+    logger.debug(f"Payment Hash: {payment_hash.hex()}")
+    logger.debug(f"CLTV expiry: {cltv_expiry}")
+    logger.debug(f"Onion length: {len(onion)}")
+
+    generated_onion = generate_new_onion(RECV_NODE_PUBKEY, amount_msat, payment_hash)
+
+    modified_payload = orig_payload[0:84]
+    modified_payload += struct.pack(le_onion, generated_onion)
+
+    return modified_payload
 
 
 def parse_message(msg: bytes, direction: str) -> bytes:
@@ -155,8 +164,8 @@ def parse_message(msg: bytes, direction: str) -> bytes:
         )
     )
 
-    # handle htlc_updates
-    if msg_code == 128:
+    # handle htlc_updates receiver
+    if msg_code == ADD_UPDATE_HTLC and direction == "Outbound":
         return msg_type + parse_update_add_htlc(msg_payload, direction)
 
     return msg_type + msg_payload
