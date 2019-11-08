22711725f4baac99a922f28620ee5b10930f310b
willcl-ark <will8clark@gmail.com>
2019-11-08 16:21:51 +0000

    print all node master privkeys
    Signed-off-by: willcl-ark <will8clark@gmail.com>

--- xpk/extract_private_key_from_hsm_secret.py
+++ xpk/extract_private_key_from_hsm_secret.py
@@ -11,69 +11,77 @@ import secp256k1
 from hkdf import Hkdf
 from lightning import LightningRpc
 
-# first thing: read the $LIGHTNING_DIR/hsm_secret
-# xxd -p ~/.lightning/hsm_secret | tr -d '\n' && echo ""
 
-hex_value = "" or sys.argv[-1]
-
-# if you have it hex-encoded (as given from the xxd line above)
-if hex_value and len(hex_value) > 60:
-    # proceed to make it a binary string again
-    from binascii import unhexlify
-
-    hsm_secret = unhexlify(hex_value)
-else:
-    # or read it directly
-
-    # hsm_secret = open(expanduser("~/.lightning/hsm_secret"), "rb").read()
-    hsm_secret = open("/tmp/l1-regtest/hsm_secret", "rb").read()
-
-# to generate the node private key, you must apply this hkdf thing
-# (which is a special way to an hmac) to id
+nodes = ["1", "2", "3"]
 
 
-salt = bytes([0]) or b"\x00"
-key = Hkdf(salt, hsm_secret, hash=hashlib.sha256).expand(b"nodeid")
-print(key.hex())
-# we're done here.
+def print_privkeys(node):
+    # first thing: read the $LIGHTNING_DIR/hsm_secret
+    # xxd -p ~/.lightning/hsm_secret | tr -d '\n' && echo ""
 
-# however, in the c-lightning code they say there's a ridiculously small chance of the
-# key produced here not being valid to the secp256k1 parameters, so they test it
-# and increase the salt until it is valid.
+    hex_value = "" or sys.argv[-1]
 
-# if for some reason your key is not correct with salt 0 you can just increase it by 1
-# and be fine (in the majority of cases you can just use salt 0 and be fine)
+    # if you have it hex-encoded (as given from the xxd line above)
+    if hex_value and len(hex_value) > 60:
+        # proceed to make it a binary string again
+        from binascii import unhexlify
 
-# how to test? I don't know, maybe secp256k1 will raise an exception if the key is wrong?
+        hsm_secret = unhexlify(hex_value)
+    else:
+        # or read it directly
 
-i = 0
-while True:
-    salt = bytes([i])
+        # hsm_secret = open(expanduser("~/.lightning/hsm_secret"), "rb").read()
+        hsm_secret = open(f"/tmp/l{node}-regtest/hsm_secret", "rb").read()
+
+    # to generate the node private key, you must apply this hkdf thing
+    # (which is a special way to an hmac) to id
+
+    salt = bytes([0]) or b"\x00"
     key = Hkdf(salt, hsm_secret, hash=hashlib.sha256).expand(b"nodeid")
-    try:
-        secp256k1.PrivateKey(key)
-        break
-    except:
-        i += 1
-print(key.hex())
+    # print(key.hex())
+    # we're done here.
 
-# maybe you want to be extra-sure. in that case you can check the public key generated
-# from the private key obtained here against the public key your node is advertising
-# to everybody.
+    # however, in the c-lightning code they say there's a ridiculously small chance of the
+    # key produced here not being valid to the secp256k1 parameters, so they test it
+    # and increase the salt until it is valid.
 
-ln = LightningRpc("/tmp/l1-regtest//lightning-rpc")
-# ln = LightningRpc(expanduser("~/.lightning/lightning-rpc"))
+    # if for some reason your key is not correct with salt 0 you can just increase it by 1
+    # and be fine (in the majority of cases you can just use salt 0 and be fine)
 
+    # how to test? I don't know, maybe secp256k1 will raise an exception if the key is wrong?
 
-i = 0
-while True:
-    salt = bytes([i])
-    key = Hkdf(salt, hsm_secret, hash=hashlib.sha256).expand(b"nodeid")
-    try:
-        privkey = secp256k1.PrivateKey(key)
-        if privkey.pubkey.serialize().hex() == ln.getinfo()["id"]:
-            # success!
+    i = 0
+    while True:
+        salt = bytes([i])
+        key = Hkdf(salt, hsm_secret, hash=hashlib.sha256).expand(b"nodeid")
+        try:
+            secp256k1.PrivateKey(key)
             break
-    except:
-        i += 1
-print(key.hex())
+        except:
+            i += 1
+    # print(key.hex())
+
+    # maybe you want to be extra-sure. in that case you can check the public key generated
+    # from the private key obtained here against the public key your node is advertising
+    # to everybody.
+
+    ln = LightningRpc(f"/tmp/l{node}-regtest/lightning-rpc")
+    # ln = LightningRpc(expanduser("~/.lightning/lightning-rpc"))
+
+    i = 0
+    while True:
+        salt = bytes([i])
+        key = Hkdf(salt, hsm_secret, hash=hashlib.sha256).expand(b"nodeid")
+        try:
+            privkey = secp256k1.PrivateKey(key)
+            if privkey.pubkey.serialize().hex() == ln.getinfo()["id"]:
+                # success!
+                break
+        except:
+            i += 1
+    print(f"Node l{node} privkey: {key.hex()}")
+
+
+if __name__ == "__main__":
+    for node in nodes:
+        print_privkeys(node)
