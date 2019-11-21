e94d9e307875bfb84924104684f3c0e8990fa927
willcl-ark <will8clark@gmail.com>
2019-11-21 19:40:44 +0000

    Working sendpay from l1 to l2!!
    Signed-off-by: willcl-ark <will8clark@gmail.com>

--- scripts/sendpay.py
+++ scripts/sendpay.py
@@ -4,6 +4,14 @@ from uuid import uuid4
 from lightning import LightningRpc, RpcError
 
 
+"""
+Challenge:
+rpc3 generates an invoice and returns details to rpc1. rpc1 then does a sendpay only to
+*rpc2*, who, upon not recognising the payment hash will generate an onion which forwards
+to rpc3.
+"""
+
+
 rpc1 = LightningRpc("/tmp/l1-regtest/lightning-rpc")
 rpc2 = LightningRpc("/tmp/l2-regtest/lightning-rpc")
 rpc3 = LightningRpc("/tmp/l3-regtest/lightning-rpc")
@@ -16,16 +24,19 @@ except RpcError:
     raise
 
 
-# rpc1 gets a route to rpc3
+# get rpc2 node_id
+rpc2_node_id = rpc2.getinfo()["id"]
+
+# rpc1 gets a route to rpc2
 try:
     route = rpc1.getroute(
-        node_id=inv["payee"], msatoshi=inv["msatoshi"], riskfactor=10, cltv=9
+        node_id=rpc2_node_id, msatoshi=inv["msatoshi"], riskfactor=10, cltv=15
     )["route"]
 except RpcError:
     raise
 
 
-# rpc1 does a sendpay to rpc3
+# rpc1 does a sendpay to rpc2
 try:
     send = rpc1.sendpay(
         route=route,
