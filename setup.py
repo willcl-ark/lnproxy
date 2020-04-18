6977c70f26b986d00fe94bbd3e0b8b141818bc1b
willcl-ark <will8clark@gmail.com>
2020-04-18 21:52:08 +0100

    Bump to v0.3.2
    Signed-off-by: willcl-ark <will8clark@gmail.com>

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="lnproxy",
-    version="0.3.1",
+    version="0.3.2",
     author="Will Clark",
     author_email="will8clark@gmail.com",
     description="A C-Lightning transport proxy",
